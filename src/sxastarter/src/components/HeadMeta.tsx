28017014e937d64358b0ce5c5ad22b7515f0bce8
Jatin Prajapati <jprajapati@horizontal.com>
2024-06-03 23:22:52 +0530

    fixed linting errors

--- src/sxastarter/src/components/HeadMeta.tsx
+++ src/sxastarter/src/components/HeadMeta.tsx
@@ -2,11 +2,13 @@ import { Field } from '@sitecore-jss/sitecore-jss-nextjs';
 import Head from 'next/head';
 import React from 'react';
 type HeadMetaProps = {
-  Title: Field<string>;
-  keywords: Field<string>;
-  metaDescription: Field<string>;
+  data: {
+    Title: Field<string>;
+    keywords: Field<string>;
+    metaDescription: Field<string>;
+  };
 };
-const HeadMeta = (data: HeadMetaProps): JSX.Element => {
+const HeadMeta = ({ data }: HeadMetaProps): JSX.Element => {
   return (
     <>
       <Head>
